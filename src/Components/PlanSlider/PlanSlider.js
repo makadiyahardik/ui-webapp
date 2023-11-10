5880c3d43965e177edce5027262445a4b7658465
hardikmakadiya <hardikmakadiya8@gmail.com>
2023-11-10 15:15:33 +0530

    added infinity slider

--- src/Components/PlanSlider/PlanSlider.js
+++ src/Components/PlanSlider/PlanSlider.js
@@ -35,7 +35,38 @@ const PlanSlider = () => {
       
       </div>
     </div>,
-    
+     <div className="md:flex flex md:flex-row flex-col-reverse justify-center items-center mx-auto w-full">
+     <div className="flex justify-center items-center md:w-1/2">
+     
+       <div>
+         {' '}
+         <img
+           src="./assets/images/iphone14.png"
+           className="h-auto md:w-[800px] w-auto"
+           alt="iphone14"
+         />
+       </div>
+     </div>
+
+     <div className="flex justify-center items-center w-full md:w-1/2">
+       <div className="flex flex-col justify-start items-start">
+         <div>
+           <div className="font-bold text-[#CBCBCB] tracking-[-0.6px] md:tracking-[-1.3px] md:text-[65px] text-[35px] md:leading-[112px] ">
+             <h1>
+               Twój spersonalizowany
+               <br />
+               <span className="text-[#8FBD12]  ">Dzienny plan 2</span>{' '}
+             </h1>
+           </div>
+           <p className="font-medium text-secondary md:text-[18px] md:leading-[38px]   text-[16px] leading-[36px] mt-[2rem]">
+             Twoja spersonalizowana lista zadań, dzięki której poprawisz
+             zdrowie i będziesz cieszyć się dłuższym życiem
+           </p>
+         </div>
+       </div>
+     
+     </div>
+   </div>,
   ];
   const responsive = {
     464: { items: 1 },
@@ -50,14 +81,14 @@ const PlanSlider = () => {
           mouseTracking
           items={items}
           responsive={responsive}
-          infinite={false}
-          disableDotsControls
+          infinite={true}
+          
           renderPrevButton={() => {
             return (
               <div className="hidden md:flex p-4 absolute left-[-4%] top-[35%] cursor-pointer md:flex hidden">
                 <img
                   src="./assets/images/leftarrow.png"
-                  className="h-auto md:w-[4rem] w-auto"
+                  className="h-auto md:w-[3rem] w-auto"
                   alt="leftarrow"
                 />
               </div>
@@ -65,10 +96,10 @@ const PlanSlider = () => {
           }}
           renderNextButton={() => {
             return (
-              <div className="hidden md:flex p-4 absolute right-[4%] top-[35%] cursor-pointer md:flex hidden">
+              <div className="hidden md:flex p-4 absolute right-0 top-[35%] cursor-pointer md:flex hidden">
                 <img
                   src="./assets/images/rightarrow.png"
-                  className="h-auto md:w-[4rem] w-auto"
+                  className="h-auto md:w-[3rem] w-auto"
                   alt="rightarrow"
                 />
               </div>
--- src/Components/Problems/Problems.js
+++ src/Components/Problems/Problems.js
@@ -1,83 +1,132 @@
 import React from 'react';
-
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 function Problems() {
-  return (
-    <div className="relative " id="Technology">
-      <div className="md:max-w-[1400px] mx-auto  justify-center items-center w-full relative  ">
-        <div className="flex flex-wrap justify-center items-center md:space-x-10 space-x-3 ">
-          <div className="">
-            <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
-              <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
-                Stany przedcukrzycowe i cukrzyca t2
-              </p>
-            </div>
-          </div>
-          <div className="my-3">
-            <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
-              <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
-                Insulinooporność
-              </p>
-            </div>
-          </div>{' '}
-          <div className="my-3">
-            <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
-              <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
-                Stany przedcukrzycowe i cukrzyca T2
-              </p>
-            </div>
-          </div>{' '}
-          <div className="my-3">
-            <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
-              <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
-                Zaburzenia lipidowe
-              </p>
-            </div>
-          </div>{' '}
-          <div className="my-3">
+
+
+  const items1=[
+    <div className="">
+    <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
+      <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
+        Stany przedcukrzycowe 
+      </p>
+    </div>
+  </div>,
+  <div className=" mx-5">
+    <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
+      <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
+        Insulinooporność
+      </p>
+    </div>
+  </div>,
+  <div className=" mx-5">
+    <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
+      <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
+        Stany przedcukrzycowe
+      </p>
+    </div>
+  </div>,
+  <div className=" mx-5">
+    <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
+      <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
+        Zaburzenia lipidowe
+      </p>
+    </div>
+  </div>,
+  
+  <div className=" mx-5">
+  <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
+    <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
+      Przedwczesne starzenie
+    </p>
+  </div>
+</div>
+  ]
+
+  const items2=[
+    <div className=" mx-5">
             <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
               <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
                 Otyłość
               </p>
             </div>
-          </div>{' '}
-          <div className="my-3">
+          </div>,
+          <div className=" mx-5">
             <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
               <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
                 Insulinooporność
               </p>
             </div>
-          </div>{' '}
-          <div className="my-3">
+          </div>,
+          <div className=" mx-5">
             <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
               <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
-                Niedoczynność tarczycy i Hashimoto
+                Niedoczynność  i Hashimoto
               </p>
             </div>
-          </div>{' '}
-          <div className="my-3">
+          </div>,
+          <div className=" mx-5">
             <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
               <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
                 Zmień nitya
               </p>
             </div>
-          </div>{' '}
-          <div className="my-3">
-            <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
-              <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
-                Przedwczesne starzenie
-              </p>
-            </div>
-          </div>{' '}
-          <div className="my-3">
+          </div>,<div className=" mx-5">
             <div className="flex justify-center items-center w-auto text-center  bg-[#F7F7F7] rounded-full p-2">
               <p className="text-[18px] font-normal leading-[19px] text-bgSeconday mx-3">
                 Zmień uva laov
               </p>
             </div>
-          </div>{' '}
-    
+          </div>
+  ]
+
+  const responsive = {
+    464: { items: 3 },
+    768: { items: 3 },
+    1024: { items: 4 },
+  };
+  return (
+    <div className="relative " id="Technology">
+      <div className="md:max-w-[1400px] mx-auto  justify-center items-center w-full relative  ">
+        <div className="flex flex-wrap justify-center items-center md:space-x-10 space-x-3 ">
+         
+        <AliceCarousel
+          mouseTracking
+          items={items1}
+          responsive={responsive}
+          infinite={true}
+          disableDotsControls
+          disableButtonsControls
+          autoPlay
+          autoPlayStrategy="none"
+          autoPlayInterval={2000}
+          animationDuration={2000}
+        />
+      
+       
+       
     
         </div>
+
+        <div className="flex mt-10 flex-wrap justify-center items-center md:space-x-10 space-x-3 ">
+         
+         <AliceCarousel
+           mouseTracking
+           items={items2}
+           responsive={responsive}
+           infinite={true}
+           disableDotsControls
+           disableButtonsControls
+           autoPlay
+           autoPlayStrategy="none"
+           autoPlayInterval={2000}
+           animationDuration={2000}
+         />
+       
+        
+        
+     
+         </div>
       </div>
     </div>
   );
